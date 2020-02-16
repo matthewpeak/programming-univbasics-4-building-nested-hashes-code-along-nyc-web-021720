110db2cc0175cfc9427e5019fe3ac0e8a4193aa6
Matthew Peak <matthewpeak@gmail.com>
2020-02-16 17:41:32 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,4 @@
+
 def bonus
   epic_tragedy = {
    :montague => {
@@ -8,13 +9,13 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
@@ -27,5 +28,5 @@ def bonus
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
- epic_tragedy
-end
+epic_tragedy
+  end
