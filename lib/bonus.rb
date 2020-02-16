e5e9e1bbc097fdefabb8c05f26284dbffe09c30c
Matthew Peak <matthewpeak@gmail.com>
2020-02-16 17:44:12 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,5 +28,5 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  bonus
 end
